0c1b5b2c2d4aeda7b61c13d348b0a09deb098209
LorenzovandenDungen <90601094+LorenzovandenDungen@users.noreply.github.com>
2024-12-07 12:58:08 +0100

    use Laravel\Sanctum\HasApiTokens; toegevoegd aan user.php en
    use App\Http\Controllers\AuthController;
    use App\Http\Controllers\PackageController;

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PackagesController;
+use App\Http\Controllers\PackageController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
